3e2616c6d494afd27c2e3202588fdab90cf444fe
ertem0 <35893360+ertem0@users.noreply.github.com>
2021-12-06 18:05:15 +0000

    onjfiles

--- src/models/Data.java
+++ src/models/Data.java
@@ -5,5 +5,10 @@ public class Data {
     protected int day;
     protected int month;
     protected int year;
-    
+
+    public Data (int d, int m, int y){
+        this.day = d;
+        this.month = m;
+        this.year = y;
+    }
 }
---
+++ src/models/list_clients.java
@@ -0,0 +1,66 @@
+package models;
+import java.io.*;
+import java.util.ArrayList;
+
+
+class list_clients {
+    
+    private ArrayList <Cliente> clientes;
+    
+    public  void read_text(){
+        File f = new File("./files/clients.txt");
+
+        if (f.exists() && f.isFile()) {
+            try {
+                FileReader fr = new FileReader(f);
+                BufferedReader br = new BufferedReader(fr);
+                String line;
+
+                while ((line = br.readLine()) != null) {
+                    String [] str =  line.split(",");
+                    Cliente cliente = new Cliente(str[0], str [1], str[2], Integer.parseInt(str[3]), new Data(Integer.parseInt(str[4]), Integer.parseInt(str[5]), Integer.parseInt(str[5])));
+                    
+                    boolean exists = false;
+                    for(Cliente cliente2: clientes){
+                        if (cliente.email ==  cliente2.email){
+                            exists = true;
+                        }
+                    }
+                    if (!exists){
+                    clientes.add(cliente);
+                    }
+                    
+                }
+                
+                br.close();
+
+
+            } catch (FileNotFoundException ex) {
+                System.out.println("Erro a abrir ficheiro de texto.");
+
+            } catch (IOException ex) {
+                System.out.println("Erro a ler ficheiro de texto.");
+            }
+        } else {
+            System.out.println("Ficheiro não existe.");
+        }
+    }
+
+    
+    
+    public void writethisClass(){
+        File f = new File("./files/ficheiro.obj");
+           
+        try {
+            FileOutputStream fos = new FileOutputStream(f);
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            oos.writeObject(clientes);
+            oos.close();
+
+        } catch (FileNotFoundException ex) {
+            System.out.println("Erro a criar ficheiro.");
+        } catch (IOException ex) {
+            System.out.println("Erro a escrever para o ficheiro.");
+        }
+    }
+}
