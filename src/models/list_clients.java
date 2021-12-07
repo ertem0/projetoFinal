bd0c01dd8cbca383e0702af549745873e33fe6ba
fsrosendo02 <fsrosendo02@gmail.com>
2021-12-07 14:57:15 +0000

    List_clients

--- src/models/list_clients.java
+++ src/models/list_clients.java
@@ -3,12 +3,13 @@ import java.io.*;
 import java.util.ArrayList;
 
 
-class list_clients {
-    
-    private ArrayList <Cliente> clientes;
-    
+public class list_clients {
+
+    private ArrayList <Cliente> clientes = new ArrayList<>();
+
+
     public  void read_text(){
-        File f = new File("./files/clients.txt");
+        File f = new File("src/models/files/clients.txt");
 
         if (f.exists() && f.isFile()) {
             try {
@@ -19,19 +20,19 @@ class list_clients {
                 while ((line = br.readLine()) != null) {
                     String [] str =  line.split(",");
                     Cliente cliente = new Cliente(str[0], str [1], str[2], Integer.parseInt(str[3]), new Data(Integer.parseInt(str[4]), Integer.parseInt(str[5]), Integer.parseInt(str[5])));
-                    
+
                     boolean exists = false;
                     for(Cliente cliente2: clientes){
-                        if (cliente.email ==  cliente2.email){
+                        if (cliente.email.equals(cliente2.email)){
                             exists = true;
                         }
+                        if (!exists){
+                            clientes.add(cliente);
+                        }
                     }
-                    if (!exists){
-                    clientes.add(cliente);
-                    }
-                    
+
                 }
-                
+                writethisClass();
                 br.close();
 
 
@@ -46,11 +47,11 @@ class list_clients {
         }
     }
 
-    
-    
+
+
     public void writethisClass(){
-        File f = new File("./files/ficheiro.obj");
-           
+        File f = new File("src/models/files/ficheiro.obj");
+
         try {
             FileOutputStream fos = new FileOutputStream(f);
             ObjectOutputStream oos = new ObjectOutputStream(fos);
